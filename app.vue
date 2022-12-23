d4e386ea78accbec32cd215b0d62690675878417
DaZiYuan <mscoder@hotmail.com>
2022-12-23 19:44:58 +0800

    测试

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    {{ date }}
+  </div>
+</template>
+<script lang="ts" setup>
+const date = ref()
+//is server side
+if (process.server) {
+  date.value = new Date()
+}
+</script>
